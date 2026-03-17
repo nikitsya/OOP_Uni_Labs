d549b9c5ed2b07258f08c5875e12e67d4d47024d
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 20:03:06 +0000

    11_design_patterns_2_exercises.ex2: add Required API

--- src/t11_design_patterns_2/exercises/ex02/Exercise.java
+++ src/t11_design_patterns_2/exercises/ex02/Exercise.java
@@ -1,7 +1,23 @@
 package t11_design_patterns_2.exercises.ex02;
 
+enum Difficulty {EASY, HARD}
+
+interface Enemy {
+    String name();
+
+    int hp();
+
+    int damage();
+}
+
+class EnemyFactory {
+    Enemy createSlime(Difficulty difficulty) {
+        return null;
+    }
+}
+
 public class Exercise {
     public static void run() {
-
+        // TODO
     }
 }
