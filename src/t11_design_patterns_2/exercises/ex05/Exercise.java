c4973f3ee8bfc0d383c2e32a7c470b5e0278aa18
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 21:41:25 +0000

    t11_design_patterns_2_exercises.ex5: add required API

--- src/t11_design_patterns_2/exercises/ex05/Exercise.java
+++ src/t11_design_patterns_2/exercises/ex05/Exercise.java
@@ -1,5 +1,25 @@
 package t11_design_patterns_2.exercises.ex05;
 
+interface Logger {
+    void log(String msg);
+}
+
+class LegacyLogger {
+    void logMessage(String msg) {
+
+    }
+}
+
+class LegacyLoggerAdapter implements Logger {
+    LegacyLoggerAdapter(LegacyLogger legacy) {
+
+    }
+
+    void log(String msg) {
+
+    }
+}
+
 public class Exercise {
     public static void run() {
         // TODO
