8a44876cc0531951e62d7a0607c255ebe7d568cb
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 21:49:20 +0000

    t11_design_patterns_2_exercises.ex5: add required API

--- src/t11_design_patterns_2/exercises/ex06/Exercise.java
+++ src/t11_design_patterns_2/exercises/ex06/Exercise.java
@@ -1,5 +1,14 @@
 package t11_design_patterns_2.exercises.ex06;
 
+interface CheckoutService { boolean checkout(int cents); }
+
+class ThirdPartyGateway { boolean makePayment(String euroAmount); }
+
+class GatewayCheckoutAdapter implements CheckoutService {
+    GatewayCheckoutAdapter(ThirdPartyGateway gateway);
+    boolean checkout(int cents);
+}
+
 public class Exercise {
     public static void run() {
         // TODO
