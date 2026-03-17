081d24f466a8290756b5d0c65d15a6062311eb23
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 20:30:29 +0000

    t11_design_patterns_2_exercises.ex4: add required API

--- src/t11_design_patterns_2/exercises/ex04/Exercise.java
+++ src/t11_design_patterns_2/exercises/ex04/Exercise.java
@@ -1,5 +1,23 @@
 package t11_design_patterns_2.exercises.ex04;
 
+interface TemperatureListener {
+    void onTemperatureChanged(double celsius);
+}
+
+class TemperatureSensor {
+    void addListener(TemperatureListener l) {
+
+    }
+
+    boolean removeListener(TemperatureListener l) {
+        return false;
+    }
+
+    void setTemperature(double celsius) {
+
+    }
+}
+
 public class Exercise {
     public static void run() {
         // TODO
