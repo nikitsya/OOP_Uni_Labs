3ef6b70e7a320981845294eac1d57181313d22ee
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 21:50:04 +0000

    t11_design_patterns_2_exercises.ex5: reformat code

--- src/t11_design_patterns_2/exercises/ex06/Exercise.java
+++ src/t11_design_patterns_2/exercises/ex06/Exercise.java
@@ -1,12 +1,23 @@
 package t11_design_patterns_2.exercises.ex06;
 
-interface CheckoutService { boolean checkout(int cents); }
+interface CheckoutService {
+    boolean checkout(int cents);
+}
 
-class ThirdPartyGateway { boolean makePayment(String euroAmount); }
+class ThirdPartyGateway {
+    boolean makePayment(String euroAmount) {
+        return false;
+    }
+}
 
 class GatewayCheckoutAdapter implements CheckoutService {
-    GatewayCheckoutAdapter(ThirdPartyGateway gateway);
-    boolean checkout(int cents);
+    GatewayCheckoutAdapter(ThirdPartyGateway gateway) {
+
+    }
+
+    public boolean checkout(int cents) {
+        return false;
+    }
 }
 
 public class Exercise {
