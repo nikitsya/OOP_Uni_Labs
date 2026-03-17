3fe43e6914c5a05c7084cc0ca719fe867d308bf8
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 20:16:10 +0000

    t11_design_patterns_2_exercises.ex3: add required API

--- src/t11_design_patterns_2/exercises/ex03/Exercise.java
+++ src/t11_design_patterns_2/exercises/ex03/Exercise.java
@@ -1,5 +1,21 @@
 package t11_design_patterns_2.exercises.ex03;
 
+interface ClickListener { void onClick(); }
+
+class Button {
+    void addListener(ClickListener l) {
+
+    }
+
+    boolean removeListener(ClickListener l) {
+        return false;
+    }
+
+    void click() {
+
+    }
+}
+
 public class Exercise {
     public static void run() {
         // TODO
