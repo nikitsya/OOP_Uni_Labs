648cf5180b7751fa2cd5b0fbd2b591fedb391e6f
nikitsya <nikitasmiichyk@gmail.com>
2026-03-17 20:14:42 +0000

    add exercises skeletons

---
+++ src/t11_design_patterns_2/exercises/ex03/Main.java
@@ -0,0 +1,7 @@
+package t11_design_patterns_2.exercises.ex03;
+
+public class Main {
+    public static void main() {
+        Exercise.run();
+    }
+}
---
+++ src/t11_design_patterns_2/exercises/ex06/Exercise.java
@@ -0,0 +1,7 @@
+package t11_design_patterns_2.exercises.ex06;
+
+public class Exercise {
+    public static void run() {
+        // TODO
+    }
+}
